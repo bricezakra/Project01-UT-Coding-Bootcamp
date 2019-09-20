93083f81663883b0ad56c681fa24f55dcbb43a2d
GCout <Gccoutret@gmail.com>
2019-09-20 13:37:53 -0500

    all info about all 4 books now comes uo in cards. It needs css badly.

--- assets/Javascript/books.js
+++ assets/Javascript/books.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   var bookApi= "AIzaSyAOMEGBcZM52PxoloGuA8EjcWPOw1fJIJs";
   var yelpApi= "Vz47dpZcuUUiQHoDUhocATeDvpK3HROrFQJn-bxpmIN9uQ1c98taQXTiYmpymZbuhSMluME66RlWDHjwKHDfwSQen-sLdTqN2siW-J_0ATdUjDW4b27AadLOisCDXXYx";
   
-  var search2= 'Sporty,Groomsman';
+  var search2= 'baking is pain';
   var bookUrl='https://www.googleapis.com/books/v1/volumes?q='+search2+'&maxResults=4&orderBy=relevance&key=' +bookApi
   
   var search =$(this).text()
@@ -33,11 +33,21 @@ $(document).ready(function(){
       $(".card-title2").text(response.items[2].volumeInfo.title);
       $(".card-title3").text(response.items[3].volumeInfo.title);
 
-      $(".book-cover0").text("src="+response.items[0].volumeInfo.imageLinks.smallThumbnail);
-      $(".book-cover1").text(response.items[1].volumeInfo.imageLinks.smallThumbnail);
-      $(".book-cover2").text(response.items[2].volumeInfo.imageLinks.smallThumbnail);
-      $(".book-cover3").text(response.items[3].volumeInfo.imageLinks.smallThumbnail);
-      }
+      $(".book-cover0").attr('src',response.items[0].volumeInfo.imageLinks.smallThumbnail);
+      $(".book-cover1").attr('src',response.items[1].volumeInfo.imageLinks.smallThumbnail);
+      $(".book-cover2").attr('src',response.items[2].volumeInfo.imageLinks.smallThumbnail);
+      $(".book-cover3").attr('src',response.items[3].volumeInfo.imageLinks.smallThumbnail);
+
+      $(".card-text0").text(response.items[0].volumeInfo.description);
+      $(".card-text1").text(response.items[1].volumeInfo.description);
+      $(".card-text2").text(response.items[2].volumeInfo.description);
+      $(".card-text3").text(response.items[3].volumeInfo.description);
+
+      $(".card-author0").text(response.items[0].volumeInfo.authors[0]);
+      $(".card-author1").text(response.items[1].volumeInfo.authors[0]);
+      $(".card-author2").text(response.items[2].volumeInfo.authors[0]);
+      $(".card-author3").text(response.items[3].volumeInfo.authors[0]);
+    }
 
     
 
@@ -53,7 +63,4 @@ $(document).ready(function(){
 
 
   })
-
-
-
-
+})
