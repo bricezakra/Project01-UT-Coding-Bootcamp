7babb3c42b3ae4eb8366c8b9bd7e72e60cf55a23
GCout <Gccoutret@gmail.com>
2019-09-20 10:40:56 -0500

    added a link to yelp.js and fixed var for link to URL search.

--- assets/Javascript/books.js
+++ assets/Javascript/books.js
@@ -1,18 +1,21 @@
 $(document).ready(function(){
+ 
+ 
   var bookApi= "AIzaSyAOMEGBcZM52PxoloGuA8EjcWPOw1fJIJs";
   var yelpApi= "Vz47dpZcuUUiQHoDUhocATeDvpK3HROrFQJn-bxpmIN9uQ1c98taQXTiYmpymZbuhSMluME66RlWDHjwKHDfwSQen-sLdTqN2siW-J_0ATdUjDW4b27AadLOisCDXXYx";
-
+  
+  var search2= 'Sporty,Groomsman';
   var bookUrl='https://www.googleapis.com/books/v1/volumes?q='+search2+'&maxResults=4&orderBy=relevance&key=' +bookApi
   
- 
-
   var search =$(this).text()
-  var search2= "dogs";
+
+  
 
   $.ajax({
     method: "GET",
     url: bookUrl,
   }).then(function(response){
+    console.log(bookUrl)
     console.log(response)
   })
 
