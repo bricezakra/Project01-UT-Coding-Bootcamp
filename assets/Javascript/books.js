f94e38fcd37f66bf1b34690adc110b7ed14f1ae1
bricezakra <bricezakra@live.com>
2019-09-20 14:49:16 -0500

    yelp.js updated

--- assets/Javascript/books.js
+++ assets/Javascript/books.js
@@ -1,23 +1,23 @@
-$(document).ready(function(){
- 
- 
-  var bookApi= "AIzaSyAOMEGBcZM52PxoloGuA8EjcWPOw1fJIJs";
-  var yelpApi= "Vz47dpZcuUUiQHoDUhocATeDvpK3HROrFQJn-bxpmIN9uQ1c98taQXTiYmpymZbuhSMluME66RlWDHjwKHDfwSQen-sLdTqN2siW-J_0ATdUjDW4b27AadLOisCDXXYx";
-  
-  var search2= 'Sporty,Groomsman';
-  var bookUrl='https://www.googleapis.com/books/v1/volumes?q='+search2+'&maxResults=4&orderBy=relevance&key=' +bookApi
-  
-  var search =$(this).text()
+$(document).ready(function() {
 
-  
 
-  $.ajax({
-    method: "GET",
-    url: bookUrl,
-  }).then(function(response){
-    console.log(bookUrl)
-    console.log(response)
-  })
+    var bookApi = "AIzaSyAOMEGBcZM52PxoloGuA8EjcWPOw1fJIJs";
+    var yelpApi = "Vz47dpZcuUUiQHoDUhocATeDvpK3HROrFQJn-bxpmIN9uQ1c98taQXTiYmpymZbuhSMluME66RlWDHjwKHDfwSQen-sLdTqN2siW-J_0ATdUjDW4b27AadLOisCDXXYx";
+
+    var search2 = 'Sporty,Groomsman';
+    var bookUrl = 'https://www.googleapis.com/books/v1/volumes?q=' + search2 + '&maxResults=4&orderBy=relevance&key=' + bookApi
+
+    var search = $(this).text()
+
+
+
+    $.ajax({
+        method: "GET",
+        url: bookUrl,
+    }).then(function(response) {
+        console.log(bookUrl)
+        console.log(response)
+    })
 
 
 
