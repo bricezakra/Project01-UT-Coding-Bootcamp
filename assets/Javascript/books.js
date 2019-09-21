62e6d11430097b177e687fede2026f1dc459fcdb
G Cout <Gccoutret@gmail.com>
2019-09-21 16:07:31 -0500

    added some styling, made descriptions scroll in a set sized box

--- assets/Javascript/books.js
+++ assets/Javascript/books.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     var bookApi = "AIzaSyAOMEGBcZM52PxoloGuA8EjcWPOw1fJIJs";
     var yelpApi = "Vz47dpZcuUUiQHoDUhocATeDvpK3HROrFQJn-bxpmIN9uQ1c98taQXTiYmpymZbuhSMluME66RlWDHjwKHDfwSQen-sLdTqN2siW-J_0ATdUjDW4b27AadLOisCDXXYx";
 
-    var search2 = 'baking is pain';
+    var search2 = 'Tacos Mexico';
     var bookUrl = 'https://www.googleapis.com/books/v1/volumes?q=' + search2 + '&maxResults=4&orderBy=relevance&key=' + bookApi
 
     var search = $(this).text()
