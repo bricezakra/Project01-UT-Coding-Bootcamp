3e9987e82485bb46084152a86b33d907f9f1275f
GCout <Gccoutret@gmail.com>
2019-09-23 12:41:57 -0500

    added to yelp.js, now it calls the correct into, will put it into html next

--- assets/Javascript/books.js
+++ assets/Javascript/books.js
@@ -6,16 +6,25 @@ $(document).ready(function() {
     var yelpApi = "Vz47dpZcuUUiQHoDUhocATeDvpK3HROrFQJn-bxpmIN9uQ1c98taQXTiYmpymZbuhSMluME66RlWDHjwKHDfwSQen-sLdTqN2siW-J_0ATdUjDW4b27AadLOisCDXXYx";
 
     var search2= "Chuck Norris";
-    var bookUrl = 'https://www.googleapis.com/books/v1/volumes?q=' + search2 + '&maxResults=4&orderBy=relevance&key=' + bookApi
+    var bookUrl = 'https://www.googleapis.com/books/v1/volumes?q=' + fullSearch + '&maxResults=4&orderBy=relevance&key=' + bookApi
 
     // @TODO: what is this getting used for? -angelica 
     // var search = $(this).text()
+    
+    var genreSearch = $("#readBookGenre").val();
+    console.log(genreSearch)
+    var authorSearch= $("#readBookAuthor").val();
+    console.log(authorSearch)
+    var fullSearch= genreSearch+authorSearch;
+    console.log(fullSearch);
+    
+
 
     $.ajax({
         method: "GET",
         url: bookUrl,
     }).then(function(response) {
-        console.log(response)
+    
         var items = response.items;
         for (var i = 0; i < items.length; i++) {
 
@@ -23,10 +32,7 @@ $(document).ready(function() {
             var author = response.items[i].volumeInfo.authors[0];
             var description = response.items[i].volumeInfo.description;
             var coverImg = response.items[i].volumeInfo.imageLinks.smallThumbnail;
-            console.log(title)
-            console.log(author)
-            console.log(description)
-            console.log(coverImg)
+            
 
             $(".card-title0").text(response.items[0].volumeInfo.title);
             $(".card-title1").text(response.items[1].volumeInfo.title);
