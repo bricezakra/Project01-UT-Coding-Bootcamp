8e850818051a8b682c93d73136f528100cd7795b
GCout <Gccoutret@gmail.com>
2019-09-23 13:22:51 -0500

    trying to dynamically post content to the html

--- assets/Javascript/yelp.js
+++ assets/Javascript/yelp.js
@@ -43,7 +43,29 @@ $(document).ready(function() {
                 //var bookStoreAddress=data.businesses[i].location.address1+","+data.businesses[i].location.city+","+data.businesses[i].location.zip_code;
                 console.log(bookStoreAlias, bookStoreAddress, bookStoreName, bookStorePhone)
                 console.log(bookStoreWebsite)
-            
+
+                $("#store-name0").text(data.businesses[0].name);
+                $("#store-name1").text(data.businesses[1].name);
+
+                $("#store-address0").text(data.businesses[0].location.display_address);
+                $("#store-address1").text(data.businesses[1].location.display_address);
+
+                $("#store-phone0").text(data.businesses[0].display_phone);
+                $("#store-phone1").text(data.businesses[1].display_phone);
+
+                $("#store-website0").text(bookStoreWebsite);
+                $("#store-website1").text(bookStoreWebsite);
+                var bookCard = $("<div>");
+                bookCard.addClass("card bookcard")
+                var cardBody = $("<div>");
+                //cardBody = <div></div>
+                var storeAddress= $("h5 class='sotre-address'>").text(data.businesses[i].location.display_address)
+                var storePhone= $("h5 class='store-phone'>").text(data.businesses[i].display_phone);
+                var storeWebsite= $("<h5 class='store-website'>").text("yelp.com/biz/"+bookStoreAlias);
+                var storeName = $("<h3 class='store-name'>").text(data.businesses[i].name);
+                cardBody.append(storeName, storePhone, storeWebsite, storeAddress)
+                bookCard.append(cardBody);
+                $(".bookDiv").append(bookCard);
             }
 
 
